d22ef74cc8a81672545b20715d49794bfa72f92c
wangchao <shift2C1@163.com>
2020-05-13 14:34:40 +0800

    服务熔断hystrix

--- payment8001/src/main/java/com/pipichao/controller/PayController.java
+++ payment8001/src/main/java/com/pipichao/controller/PayController.java
@@ -3,6 +3,7 @@ package com.pipichao.controller;
 import com.pipichao.service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,14 @@ public class PayController {
     @RequestMapping("payOrderTimeOut")
     public String payOrderFromFeignClientTimeOut(){
         return payService.payOrderFromFeignClientTimeOut();
+    }
+
+    /**
+     * 服务熔断
+     *
+     * */
+    @RequestMapping(value = "/circuitbreaker/{id}")
+    public String circuitbreaker(@PathVariable("id")Integer id){
+        return payService.circuitBreaker(id);
     }
 }
--- payment8001/src/main/java/com/pipichao/service/PayService.java
+++ payment8001/src/main/java/com/pipichao/service/PayService.java
@@ -1,8 +1,11 @@
 package com.pipichao.service;
 
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.config.HystrixCommandConfiguration;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,11 @@ public class PayService {
         return "支付成功:"+port;
     }
 
+    /**
+     * 服务降级
+     *
+     *
+     * */
 //    被调用方开启服务降级保护
 //    @HystrixCommand(fallbackMethod = "payOrderTimeoutHandler",commandProperties = {
 //            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "2000")//超时时间
@@ -50,4 +58,60 @@ public class PayService {
     public String defaultFallBack(){
         return "全局fallback";
     }
+
+
+    /**
+     * 服务熔断：
+     * 原理：在错误窗口期内，如果请求异常的次数和总请求次数的比率超过设置的错误比率阈值，贼hystrix断路器自动跳闸
+     *      *
+     *      *  一段时间后，再次恢复调用链路
+     *
+     *  HystrixCommandProperties:每个参数的具体位置所在类
+     *
+     *
+     * */
+    @HystrixCommand(
+            fallbackMethod = "circuitBreakerFallback",
+            commandProperties ={
+                    @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),//是否开启断路器
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//在时间窗口期的请求次数
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//时间窗口期：单位毫秒
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")//错误请求的失败比率
+
+            }
+    )
+    public String circuitBreaker(Integer id){
+        if (id.intValue()<0){
+            //模拟服务器异常
+            log.info("参数："+id+"服务器异常");
+            throw new RuntimeException("服务器异常");
+        }
+        log.info("参数："+id+"正常访问");
+        return "服务没被熔断。。";
+    }
+    public String circuitBreakerFallback(Integer id){
+        log.info("参数："+id+"服务器异常，被熔断");
+        return "服务被熔断了。。。";
+    }
+    /*
+    * 参数：1服务器异常，被熔断
+2020-05-13 13:37:02.689  INFO 3472 --- [nio-8001-exec-6] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:03.284  INFO 3472 --- [nio-8001-exec-7] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:03.754  INFO 3472 --- [nio-8001-exec-8] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:04.274  INFO 3472 --- [nio-8001-exec-9] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:04.738  INFO 3472 --- [io-8001-exec-10] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:05.290  INFO 3472 --- [nio-8001-exec-2] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:08.739  INFO 3472 --- [x-PayService-10] com.pipichao.service.PayService          : 参数：-1服务器异常
+2020-05-13 13:37:08.740  INFO 3472 --- [x-PayService-10] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:09.914  INFO 3472 --- [nio-8001-exec-1] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:10.330  INFO 3472 --- [nio-8001-exec-5] com.pipichao.service.PayService          : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:10.786   : 参数：-1服务器异常，被熔断
+2020-05-13 13:37:18.459   : 参数：1服务器异常，被熔断  ：被熔断之后即使正常访问也被降级
+2020-05-13 13:37:18.963   : 参数：1正常访问 ：过了一段时间 恢复闭合状态
+2020-05-13 13:37:19.485   : 参数：1正常访问
+2020-05-13 13:37:20.570   : 参数：1正常访问
+    * */
+
+
+
 }
